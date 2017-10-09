154fee11e2d67afba81b4c995f111c9fbf9c000c
Sébastien Viande <sviande@gmail.com>
2017-10-09 08:40:49 +0200

    Entry: init struct

---
+++ entry.go
@@ -0,0 +1,57 @@
+package goWallabag
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type EntriesResponse struct {
+	Page     int
+	Limit    int
+	Pages    int
+	Total    int
+	Links    Links    `json:"_links"`
+	Embedded Embedded `json:"_embedded"`
+}
+
+type Embedded struct {
+	Entries []Entry `json:"items"`
+}
+
+type Links struct {
+	Self  Link `json:"self"`
+	Last  Link `json:"last"`
+	First Link `json:"first"`
+	Next  Link `json:"next"`
+}
+
+type Link struct {
+	Href string `json:"href"`
+}
+
+type Entry struct {
+	IsArchived  int      `json:"is_archived"`
+	IsStarred   int      `json:"is_starred"`
+	UserName    string   `json:"user_name"`
+	UserEmail   string   `json:"user_email"`
+	UserID      int      `json:"user_id"`
+	Tags        []Tags   `json:"tags"`
+	ID          int      `json:"id"`
+	Title       string   `json:"title"`
+	URL         string   `json:"url"`
+	CreatedAt   Time     `json:"created_at"`
+	UpdatedAt   Time     `json:"updated_at"`
+	Annotations []string `json:"annotations"`
+	MimeType    string   `json:"mimetype"`
+	Language    string   `json:"language"`
+	ReadingTime int      `json:"reading_time"`
+	DomainName  string   `json:"domain_name"`
+	HTTPStatus  string   `json:"http_status"`
+	Content     string   `json:"content"`
+	Links       Links    `json:"_links"`
+}
+
+type EntriesRequest url.Values
---
+++ entry_test.go
@@ -0,0 +1,117 @@
+package goWallabag
+
+import (
+	"bytes"
+	"io/ioutil"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestParse(t *testing.T) {
+	file := "test/entries.json"
+	in, err := ioutil.ReadFile(file)
+	if err != nil {
+		t.Errorf("Missing test file %s", file)
+		return
+	}
+
+	got, err := parseEntries(bytes.NewReader(in))
+	if err != nil {
+		t.Errorf("err %s", err)
+		return
+	}
+
+	want := getWanted()
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("Parse() got \n%q, want\n%q", got, want)
+	}
+}
+
+func getWanted() EntriesResponse {
+	createdAt0, _ := time.Parse(TimeFormat, "2017-09-18T10:45:58+0200")
+	updatedAt0, _ := time.Parse(TimeFormat, "2017-09-18T18:55:26+0200")
+
+	createdAt1, _ := time.Parse(TimeFormat, "2017-09-18T10:45:49+0200")
+	updatedAt1, _ := time.Parse(TimeFormat, "2017-09-18T18:47:44+0200")
+
+	return EntriesResponse{
+		Page:  1,
+		Limit: 30,
+		Total: 1438,
+		Pages: 48,
+		Links: Links{
+			Self: Link{
+				Href: "self",
+			},
+			Last: Link{
+				Href: "last",
+			},
+			First: Link{
+				Href: "first",
+			},
+			Next: Link{
+				Href: "next",
+			},
+		},
+		Embedded: Embedded{
+			Entries: []Entry{
+				Entry{
+					IsArchived: 1,
+					IsStarred:  0,
+					UserName:   "example",
+					UserEmail:  "example@gmail.com",
+					UserID:     1,
+					Tags: []Tags{
+						Tags{
+							ID:    3,
+							Label: "Code",
+							Slug:  "code",
+						},
+					},
+					ID:          1788,
+					Title:       "Title 1788",
+					URL:         "https://blog.octo.com/le-demi-cercle-episode-3-communication-breakdown/",
+					CreatedAt:   Time{createdAt0},
+					UpdatedAt:   Time{updatedAt0},
+					Annotations: []string{},
+					MimeType:    "text/html",
+					Language:    "fr-FR",
+					ReadingTime: 8,
+					DomainName:  "blog.octo.com",
+					HTTPStatus:  "200",
+					Links: Links{
+						Self: Link{
+							Href: "/api/entries/1788",
+						},
+					},
+				},
+				Entry{
+					IsArchived:  0,
+					IsStarred:   1,
+					UserName:    "example",
+					UserEmail:   "example@gmail.com",
+					UserID:      1,
+					Tags:        []Tags{},
+					ID:          1787,
+					Title:       "Le demi-cercle (épisode 2 — Voir / Avancer)",
+					URL:         "https://blog.octo.com/le-demi-cercle-episode-2-voir-avancer/",
+					CreatedAt:   Time{createdAt1},
+					UpdatedAt:   Time{updatedAt1},
+					Annotations: []string{},
+					MimeType:    "text/html",
+					Language:    "fr-FR",
+					ReadingTime: 8,
+					DomainName:  "blog.octo.com",
+					HTTPStatus:  "200",
+					Links: Links{
+						Self: Link{
+							Href: "/api/entries/1787",
+						},
+					},
+				},
+			},
+		},
+	}
+}
