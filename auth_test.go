c4d45a7db378ab731ac19672d05275bd66da2e98
Sébastien Viande <sviande@gmail.com>
2017-10-12 08:21:15 +0200

    Auth: request add test for GetUrlValues

--- auth_test.go
+++ auth_test.go
@@ -1,8 +1,33 @@
 package goWallabag
 
 import (
+	"net/url"
+	"reflect"
 	"testing"
 )
+
+func TestGetUrlValues(t *testing.T) {
+
+	expected := url.Values{}
+	expected.Set("grant_type", "GrantType")
+	expected.Set("client_id", "ClientID")
+	expected.Set("client_secret", "ClientSecret")
+	expected.Set("username", "Username")
+	expected.Set("password", "Password")
+
+	authR := AuthRequest{}
+	authR.GrantType = "GrantType"
+	authR.ClientID = "ClientID"
+	authR.ClientSecret = "ClientSecret"
+	authR.Username = "Username"
+	authR.Password = "Password"
+
+	got := authR.GetUrlValues()
+
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("Failed for getUrlValues expected %v got %v", expected, got)
+	}
+}
 
 func TestGetHeader(t *testing.T) {
 	authResponse := AuthResponse{
