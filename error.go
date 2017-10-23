bd0239ff6530d1da85656c8464d0e24f1d6122cb
Sébastien Viande <sviande@gmail.com>
2017-10-23 19:32:11 +0200

    Error: add comment on struc

--- error.go
+++ error.go
@@ -1,5 +1,6 @@
 package goWallabag
 
+//ErrorResponse struct represent error response from wallabag API
 type ErrorResponse struct {
 	Error       string `json:"error"`
 	Description string `json:"error_description"`
