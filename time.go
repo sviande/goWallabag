512bd7baebfe3c3f25fe83f4193987e62a80eeea
Sébastien Viande <sviande@gmail.com>
2017-10-23 19:45:07 +0200

    Time: Add doc description for const, struct and func

--- time.go
+++ time.go
@@ -4,12 +4,15 @@ import (
 	"time"
 )
 
+//TimeFormat time format use by API
 const TimeFormat = "2006-01-02T15:04:05-0700"
 
+//Time struct use for specific JSON Unmarsahl cause of format
 type Time struct {
 	time.Time
 }
 
+//UnmarshalJSON func use for parsing time with TimeFormat
 func (t *Time) UnmarshalJSON(timeByte []byte) error {
 	timeStr := string(timeByte)
 	timeStr = timeStr[1 : len(timeStr)-1]
